ebd79380d481c1dbbeca705fa3dce2548f2ed720
Robby <robby@santoslab.org>
2024-10-27 08:37:18 -0500

    Fixed file permission when running in Windows.

--- build.sh
+++ build.sh
@@ -16,6 +16,7 @@ git checkout $P7ZIP_V
 git grep -lz "GetLastError" | xargs -0 sed -i'' -e 's/GetLastError/Get7zLastError/g'
 git grep -lz "SetLastError" | xargs -0 sed -i'' -e 's/SetLastError/Set7zLastError/g'
 git grep -lz "-Werror" | xargs -0 sed -i'' -e 's/-Werror//g'
+git grep -lz "if (attrib & FILE_ATTRIBUTE_UNIX_EXTENSION)" | xargs -0 sed -i'' -e 's/if (attrib \& FILE_ATTRIBUTE_UNIX_EXTENSION)/if ((getenv("COMSPEC") == NULL) \&\& (attrib \& FILE_ATTRIBUTE_UNIX_EXTENSION))/g'
 cd CPP/7zip/Bundles/Alone2
 make -j -f makefile.gcc
 cp _o/7zz $GITHUB_WORKSPACE
